514ad1aef55dd8645fb04bf49fa13d9c156de26a
Brian Yamasaki <brian.yamasaki@gmail.com>
2025-04-15 08:35:44 -0700

    Phone Number QR code

---
+++ src/components/Phone-info.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import QRcode from './Qrcode';
+import SizeInfo from './Size-info';
+
+const PhoneInfo = () => {
+  const [phoneNumber, setUrlText] = useState("");
+  const [qrText, setQrText] = useState("");
+  const [size, setSize] = useState(128);
+  
+  const onChangePhoneNumber = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setQrText("");
+    setUrlText(e.target.value);
+  }
+  const changeSize = (i: number) => {
+    setSize(i);
+  }
+  const stripNonDigits = () => (phoneNumber.replace(/\(|\)|-|_/g, ''));
+
+  return (
+    <div className="info-pane">
+      <fieldset>
+        <div className="formGroup">
+          <label htmlFor="phoneText">Phone number: </label>
+          <input id="phoneText" type="text" value={phoneNumber} onChange={onChangePhoneNumber} placeholder="Enter phone number"/>
+        </div>
+        <div className="formGroup">
+          <SizeInfo returnSize={changeSize} />
+        </div>
+        <button onClick={() => setQrText(`tel:${stripNonDigits()}`)} disabled={!phoneNumber}>Generate QR Code</button>
+      </fieldset>
+      <QRcode qrText={qrText} size={size}/>
+    </div>
+  )
+}
+
+export default PhoneInfo;
